0f9e59184a888f557abcdd284575310788d876b9
luizly <108622654+luizly@users.noreply.github.com>
2022-07-22 16:27:52 +0800

    Update ProductsPage.js
    Added in currency table

--- src/Pages/ProductsPage.js
+++ src/Pages/ProductsPage.js
@@ -1,13 +1,39 @@
 import React from 'react'
 import CardContainer from '../Components/Content/CardContainer'
 import NavBar1 from '../Components/NavBars/NavBar1'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Table from 'react-bootstrap/Table';
 
 const Products = () => {
   return (
     <div>
         <NavBar1 />
         <h1 style={{padding:'1rem'}}>Products</h1>
-        <CardContainer />
+        <Container>
+        <Row>
+        <Col><CardContainer /></Col>
+        <Col><Table striped bordered hover>
+ <thead>
+ <tr>
+ <th>Currency</th>
+ <th>Rate</th>
+ </tr>
+ </thead>
+ <tbody>
+ <tr>
+ <td>CAD</td>
+ <td>0.9225</td>
+ </tr> 
+ <tr>
+ <td>CNH</td>
+ <td>4.7868</td>
+ </tr> 
+ </tbody>
+</Table></Col>
+        </Row>
+        </Container>
     </div>
   )
 }
